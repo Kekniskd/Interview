7e15e6bff6f765f0330798f0904b611d83e0589d
kedar <kekniskedar@gmail.com>
2023-11-16 15:40:25 +0530

    highiq.py 2nd round

--- highiq.py
+++ highiq.py
@@ -10,3 +10,64 @@ def check(value):
 
 
 print(check(s))
+
+######################################## Snake and ladder game ###########################
+class Board(Player):
+    def __inti__(self, player_count):
+        self.player_count = player_count
+        self.player_list = []
+
+        for id in range(player_count):
+            new_player = Player(id)
+            player_list.append(new_player)
+
+        self.board_len = 100
+        self.stairs = {2: 15, 20: 56}
+        self.snakes = {19: 3, 40: 26}
+
+    def stair(self, player):
+        if player.position in self.stairs.keys():
+            player.position = self.stairs[player.position]
+
+    def snake(self, player):
+        if player.position in self.snakes.keys():
+            player.position = self.snakes[player.position]
+
+
+board = Board(4)
+
+
+
+class Player:
+    def __init__(self, id):
+        self.position = 0
+        self.id = id
+
+    def roll_dice(self):
+        number = random int between 1 - 6
+        if number == 6:
+
+        return number
+
+
+
+def main():
+    curr_player = 0
+    while self.player_list[0] != 100 or self.player_list[1] != 100 or self.player_list[2] != 100 or self.player_list[
+        3] != 100:
+        dice_count = self.player_list[curr_player].roll_dice()
+        self.player_list[curr_player].position = self.player_list[curr_player].position + dice_count
+        stair(self.player_list[curr_player])
+        snake(self.player_list[curr_player])
+        curr_player += 1
+        if curr_player == 5:
+            curr_player = 0
+
+    return player
+    with position 100
+
+
+
+# docker compose
+# copy.dockerimageID
+# sh
